4445dd20dd355cf9d13cb1c060222ce01e76cb4c
Jon Hadfield <jon@lessknown.co.uk>
2017-03-18 17:01:50 +0000

    initial travis setup.

--- main.go
+++ main.go
@@ -8,12 +8,14 @@ import (
 	"log"
 	"os"
 	"time"
+	"strings"
+	"encoding/base64"
 )
 
 var (
 	_debug = log.New(ioutil.Discard, "", 0)
-	_info  = log.New(ioutil.Discard, "", 0)
-	_warn  = log.New(ioutil.Discard, "", 0)
+	_info = log.New(ioutil.Discard, "", 0)
+	_warn = log.New(ioutil.Discard, "", 0)
 	_error = log.New(ioutil.Discard, "", 0)
 )
 
@@ -22,7 +24,7 @@ type users struct {
 		Name        string
 		Gecos       string
 		Public_keys []string
-		HomeDir     string
+		Home_dir    string
 		Shell       string
 	}
 }
@@ -48,11 +50,10 @@ func initLogger(level string) {
 }
 
 func main() {
-	fmt.Println("START")
 	app := cli.NewApp()
 	app.EnableBashCompletion = true
 
-	app.Name = "uman"
+	app.Name = "validatesshusers"
 	app.Version = "0.0.1"
 	app.Compiled = time.Now()
 	app.Authors = []cli.Author{
@@ -67,14 +68,6 @@ func main() {
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:  "path, p",
-			Usage: "user file path",
-		},
-		cli.BoolFlag{
-			Name:  "check, c",
-			Usage: "check the user file",
-		},
-		cli.StringFlag{
 			Name:  "log-level",
 			Usage: "Set log level (debug, info, warn, error)",
 			Value: "warn",
@@ -82,32 +75,69 @@ func main() {
 	}
 	app.Action = func(c *cli.Context) error {
 		initLogger(c.String("log-level"))
-		fmt.Println(c.String("path"))
-
-		if _, err := os.Stat(c.String("path")); err == nil {
-			fmt.Println("Got file")
-			file, err := os.Open(c.String("path"))
+		var usersFile = c.Args().Get(0)
+		if _, err := os.Stat(usersFile); err == nil {
+			file, err := os.Open(usersFile)
 			if err != nil {
-				fmt.Println("OOPS")
+				return err
 			}
-
 			defer file.Close()
-			userFile, err := ioutil.ReadFile(c.String("path"))
-			fmt.Println(string(userFile))
+			userFile, err := ioutil.ReadFile(usersFile)
 			if err != nil {
 				log.Printf("yamlFile.Get err   #%v ", err)
 			}
 			var users users
 			err = yaml.Unmarshal(userFile, &users)
 			if err != nil {
+				fmt.Println("\nSpecified file is not a valid users file.")
 				log.Fatalf("Unmarshal: %v", err)
 			} else {
-				for _, m := range users.Users {
-					fmt.Printf("%+v\n\n", m)
+				var allOk bool = true
+				for _, user := range users.Users {
+					_debug.Printf("%+v\n\n", user)
+					// Check no upper case characters in username
+					if user.Name != strings.ToLower(user.Name) {
+						fmt.Printf("Warning: user: \"%s\". Usernames should only contain lower case characters\n", user.Name)
+					}
+					// If home directory is specified, warn if not default
+					if user.Home_dir != "" && user.Home_dir != "/home/" + user.Name {
+						fmt.Printf("Warning: Specified home directory: %s differs from expected default: /home/%s\n", user.Home_dir, user.Name)
+						allOk = false
+					}
+					// Check gecos is specified and contains email address
+					if user.Gecos == "" {
+						fmt.Printf("Error: gecos (comment) not specified for user: %s\n", user.Name)
+						allOk = false
+						// Check gecos contains email address
+					} else if ! strings.Contains(user.Gecos, "@") {
+						fmt.Printf("Error: gecos (comment) for user: %s does not have an email address\n", user.Name)
+						allOk = false
+					}
+					// Check user has public keys defined
+					if len(user.Public_keys) == 0 {
+						fmt.Printf("Error: user: %s has no public keys specified\n", user.Name)
+					} else {
+						// Validate public keys
+						// TODO: Improve validation
+						for _, b64PublicKey := range user.Public_keys {
+							publicKey, _ := base64.StdEncoding.DecodeString(b64PublicKey)
+							var invalidKeys int = 0
+							if ! strings.HasPrefix(string(publicKey), "ssh-rsa ") {
+								invalidKeys++
+							}
+							if invalidKeys > 0 {
+								fmt.Printf("Error: user: %s has %d invalid public keys\n", user.Name, invalidKeys)
+							}
+						}
+					}
+
+				}
+				if allOk {
+					fmt.Println("OK")
 				}
 			}
 		} else {
-			fmt.Printf("Could not find file: %s\n", c.String("path"))
+			fmt.Printf("Could not find file: %s\n", usersFile)
 		}
 
 		return nil
