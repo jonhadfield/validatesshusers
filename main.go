0fb5c5a826fdea0091221238bc9cfa6e0145e317
Jon Hadfield <jon@lessknown.co.uk>
2017-03-18 19:31:17 +0000

    switch from urfave to kingpin.

--- main.go
+++ main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
-	"github.com/urfave/cli"
+	"gopkg.in/alecthomas/kingpin.v2"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
-	"time"
 )
 
 var (
@@ -49,108 +48,95 @@ func initLogger(level string) {
 
 }
 
+var (
+	debug = kingpin.Flag("debug", "Enable debug mode.").Bool()
+	file  = kingpin.Arg("file", "Users YAML file.").String()
+)
+
 func main() {
-	app := cli.NewApp()
-	app.EnableBashCompletion = true
-
-	app.Name = "validatesshusers"
-	app.Version = "0.0.12"
-	app.Compiled = time.Now()
-	app.Authors = []cli.Author{
-		cli.Author{
-			Name:  "Jon Hadfield",
-			Email: "jon@lessknown.co.uk",
-		},
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+	if *debug {
+		initLogger("debug")
 	}
-	app.HelpName = "validatesshusers"
-	app.Usage = "Validation for users YAML files"
-	//app.Description = "ss"
-
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "log-level",
-			Usage: "Set log level (debug, info, warn, error)",
-			Value: "warn",
-		},
+	if *file == "" {
+		kingpin.Usage()
 	}
-	app.Action = func(c *cli.Context) error {
-		initLogger(c.String("log-level"))
-		var usersFile string = c.Args().Get(0)
-		if usersFile == "" {
-			fmt.Println("USAGE  validatesshuser <file>")
-			os.Exit(1)
+
+	var usersFile = *file
+	if usersFile == "" {
+		fmt.Println("USAGE  validatesshuser <file>")
+		os.Exit(1)
+	}
+
+	if _, err := os.Stat(usersFile); err == nil {
+		file, err := os.Open(usersFile)
+		if err != nil {
+			return
 		}
-
-		if _, err := os.Stat(usersFile); err == nil {
-			file, err := os.Open(usersFile)
-			if err != nil {
-				return err
-			}
-			defer file.Close()
-			userFile, err := ioutil.ReadFile(usersFile)
-			if err != nil {
-				log.Printf("yamlFile.Get err   #%v ", err)
-			}
-			var users users
-			err = yaml.Unmarshal(userFile, &users)
-			if err != nil {
-				fmt.Println("\nSpecified file is not a valid users file.")
-				log.Fatalf("Unmarshal: %v", err)
-			} else {
-				var allOk bool = true
-				for _, user := range users.Users {
-					_debug.Printf("%+v\n\n", user)
-					// Check no upper case characters in username
-					if user.Name != strings.ToLower(user.Name) {
-						fmt.Printf("Warning: user: \"%s\". Usernames should only contain lower case characters\n", user.Name)
-					}
-					// If home directory is specified, warn if not default
-					if user.Home_dir != "" && user.Home_dir != "/home/"+user.Name {
-						fmt.Printf("Warning: Specified home directory: %s differs from expected default: /home/%s\n", user.Home_dir, user.Name)
-						allOk = false
-					}
-					// Check gecos is specified and contains email address
-					if user.Gecos == "" {
-						fmt.Printf("Error: gecos (comment) not specified for user: %s\n", user.Name)
-						allOk = false
-						// Check gecos contains email address
-					} else if !strings.Contains(user.Gecos, "@") {
-						fmt.Printf("Error: gecos (comment) for user: %s does not have an email address\n", user.Name)
-						allOk = false
-					}
-					// Check user has public keys defined
-					if len(user.Public_keys) == 0 {
-						fmt.Printf("Error: user: %s has no public keys specified\n", user.Name)
-					} else {
-						// Validate public keys
-						// TODO: Improve validation
-						for _, b64PublicKey := range user.Public_keys {
-							publicKey, _ := base64.StdEncoding.DecodeString(b64PublicKey)
-							var invalidKeys int = 0
-							if !strings.HasPrefix(string(publicKey), "ssh-rsa ") {
-								invalidKeys++
-							}
-							if invalidKeys > 0 {
-								fmt.Printf("Error: user: %s has %d invalid public keys\n", user.Name, invalidKeys)
-							}
+		defer file.Close()
+		userFile, err := ioutil.ReadFile(usersFile)
+		if err != nil {
+			log.Printf("yamlFile.Get err   #%v ", err)
+		}
+		var users users
+		err = yaml.Unmarshal(userFile, &users)
+		if err != nil {
+			fmt.Println("\nSpecified file is not a valid users file.")
+			log.Fatalf("Unmarshal: %v", err)
+		} else {
+			var allOk = true
+			for _, user := range users.Users {
+				_debug.Printf("%+v\n\n", user)
+				// Check no upper case characters in username
+				if user.Name != strings.ToLower(user.Name) {
+					fmt.Printf("Warning: user: \"%s\". Usernames should only contain lower case characters\n", user.Name)
+				}
+				// If home directory is specified, warn if not default
+				if user.Home_dir != "" && user.Home_dir != "/home/"+user.Name {
+					fmt.Printf("Warning: Specified home directory: %s differs from expected default: /home/%s\n", user.Home_dir, user.Name)
+					allOk = false
+				}
+				// Check gecos is specified and contains email address
+				if user.Gecos == "" {
+					fmt.Printf("Error: gecos (comment) not specified for user: %s\n", user.Name)
+					allOk = false
+					// Check gecos contains email address
+				} else if !strings.Contains(user.Gecos, "@") {
+					fmt.Printf("Error: gecos (comment) for user: %s does not have an email address\n", user.Name)
+					allOk = false
+				}
+				// Check user has public keys defined
+				if len(user.Public_keys) == 0 {
+					fmt.Printf("Error: user: %s has no public keys specified\n", user.Name)
+				} else {
+					// Validate public keys
+					// TODO: Improve validation
+					for _, b64PublicKey := range user.Public_keys {
+						publicKey, _ := base64.StdEncoding.DecodeString(b64PublicKey)
+						var invalidKeys int
+						if !strings.HasPrefix(string(publicKey), "ssh-rsa ") {
+							invalidKeys++
+						}
+						if invalidKeys > 0 {
+							fmt.Printf("Error: user: %s has %d invalid public keys\n", user.Name, invalidKeys)
 						}
 					}
+				}
 
-				}
-				if allOk {
-					fmt.Println("OK")
-					os.Exit(0)
-				} else {
-					os.Exit(1)
-				}
 			}
-		} else {
-			fmt.Printf("Could not open: %s\n", usersFile)
-			os.Exit(1)
+			if allOk {
+				fmt.Println("SYNTAX OK")
+				os.Exit(0)
+			} else {
+				os.Exit(1)
+			}
 		}
-
-		return nil
+	} else {
+		fmt.Printf("Could not open: %s\n", usersFile)
+		os.Exit(1)
 	}
-	app.Run(os.Args)
+
+	return
 
 }
