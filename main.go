553baa72e3844ae04495a4c393a2c7aeebe6f3c4
Jon Hadfield <jon@lessknown.co.uk>
2017-03-18 17:14:40 +0000

    bump version for deploy.

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 	app.EnableBashCompletion = true
 
 	app.Name = "validatesshusers"
-	app.Version = "0.0.3"
+	app.Version = "0.0.4"
 	app.Compiled = time.Now()
 	app.Authors = []cli.Author{
 		cli.Author{
